d62696051312f113580a679dd1892d302f19b987
Sahil Gurnani <122155998+SaHILL87@users.noreply.github.com>
2023-09-15 09:52:47 +0530

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: { colors: { accent: "#2F81F7", bgclor: "#0D1117" } },
   },
   plugins: [],
-}
-
+};
